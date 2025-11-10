90549a108bac60689058c90b801b46121a9c5ab5
Valentinvallejo5 <vvallejo@udesa.edu.ar>
2025-11-09 23:24:11 -0300

    modifificando navigation

--- src/navigation/TabNavigation.js
+++ src/navigation/TabNavigation.js
@@ -2,17 +2,35 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { FontAwesome } from '@expo/vector-icons';
 
+import ComentariosNavigation from '../navigation/ComentariosNavigation';
 import Profile from '../screens/Profile';
- 
+import NewPost from '../screens/NewPost';
 
 const Tab = createBottomTabNavigator();
 
 export default function TabNavigation() {
   return (
-    <Tab.Navigator screenOptions={{ tabBarShowLabel: false }}>
-      
-      
-      
+    <Tab.Navigator
+      screenOptions={{
+        tabBarShowLabel: false,
+        headerShown: false, 
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={ComentariosNavigation}
+        options={{
+          tabBarIcon: () => <FontAwesome name="home" size={24} color="black" />,
+        }}
+      />
+      <Tab.Screen
+        name="NewPost"
+        component={NewPost}
+        options={{
+          tabBarIcon: () => <FontAwesome name="plus-square" size={24} color="black" />,
+        }}
+      />
+
       <Tab.Screen
         name="Profile"
         component={Profile}
@@ -20,7 +38,6 @@ export default function TabNavigation() {
           tabBarIcon: () => <FontAwesome name="user" size={24} color="black" />,
         }}
       />
-      
     </Tab.Navigator>
   );
 }
